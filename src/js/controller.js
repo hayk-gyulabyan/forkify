cae67a332a79b6a8f15767eabe26fb6747002f0d
hayk-gyulabyan <gyulabyanh@gmail.com>
2022-11-04 23:30:58 +0400

    add update servings functionality

--- src/js/controller.js
+++ src/js/controller.js
@@ -19,6 +19,7 @@ const controlRecipes = async function() {
 
     // 2) Rendering recipe
     recipeView.render(model.state.recipe);
+    console.log(model.state.recipe);
   } catch (err) {
     recipeView.renderError();
   }
@@ -53,8 +54,17 @@ const controlPagination = function(goToPage) {
   paginationView.render(model.state.search);
 };
 
+const controlServings = function(newServings) {
+  // Update the recipe servings (in state)
+  model.updateServings(newServings);
+
+  // Update the recipe view
+  recipeView.render(model.state.recipe);
+};
+
 const init = function() {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
