32e5ae8e993d93907573792bc49119f928388c54
hayk-gyulabyan <gyulabyanh@gmail.com>
2022-11-06 11:46:57 +0400

    add uploading new recipe functionality

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '7707e2cd-cf19-40fa-a7e1-d8ae3e0111be';
+export const MODAL_CLOSE_SEC = 2.5;
